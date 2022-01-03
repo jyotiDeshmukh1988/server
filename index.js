ff086dadc9b7f4b175742dd7681dcd974cd6b73f
Jyoti Deshmukh <test8600896492@gmail.com>
2022-01-03 23:00:58 +0530

    new changes for the node backend

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ const app = express();
 const port = 5000;
 
 const product = require("./api/product/product");
-app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Hello from Server" });
-});
+app.use(bodyParser.json());
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.use("/api/product", product);
 
+app.get("/", (req, res) => {
+  res.json({ message: "ok" });
+});
+
 app.listen(port, () => {
-  console.log(`Now listening on port ${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
